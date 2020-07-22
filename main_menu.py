e595bf31a283c5ec645d927deead4c25d5f67d1f
Graham Keane <40239414+GrahamJamesKeane@users.noreply.github.com>
2020-07-22 05:34:00 +0100

    Updated menu options

--- main_menu.py
+++ main_menu.py
@@ -1,6 +1,9 @@
 from keyword_popularity import modules_all, overview_all, learning_outcomes_all
 from topic_stats import get_stats
 
+message_1 = "Processing... Please Wait"
+message_2 = "Process Complete"
+
 
 # Start here:
 def menu():
@@ -8,7 +11,8 @@ def menu():
         user_input_1 = int(input("""    Main Menu
     1: Keyword Analysis
     2: Topic Stats
-    3: Exit
+    3: Run All
+    4: Exit
     """))
         if user_input_1 == 1:
             user_input_2 = int(input("""    Keyword Analysis Menu
@@ -19,19 +23,34 @@ def menu():
     5: Exit
     """))
             if user_input_2 == 1:
+                print(message_1)
                 modules_all()
+                print(message_2)
             elif user_input_2 == 2:
+                print(message_1)
                 overview_all()
+                print(message_2)
             elif user_input_2 == 3:
+                print(message_1)
                 learning_outcomes_all()
+                print(message_2)
             elif user_input_2 == 4:
                 menu()
                 return
             elif user_input_2 == 5:
                 exit(0)
         elif user_input_1 == 2:
+            print(message_1)
             get_stats()
+            print(message_2)
         elif user_input_1 == 3:
+            print(message_1)
+            modules_all()
+            overview_all()
+            learning_outcomes_all()
+            get_stats()
+            print(message_2)
+        elif user_input_1 == 4:
             exit(0)
 
 
